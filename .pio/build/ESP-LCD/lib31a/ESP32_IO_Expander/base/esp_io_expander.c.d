a08dcb6eb2d25ebed5ae4d97dae578b838c8fb73
Ifousoft <ifousoft2@gmail.com>
2024-10-09 20:57:24 +0200

    message

---
+++ .pio/build/ESP-LCD/lib31a/ESP32_IO_Expander/base/esp_io_expander.c.d
@@ -0,0 +1,14 @@
+.pio/build/ESP-LCD/lib31a/ESP32_IO_Expander/base/esp_io_expander.c.o: \
+ .pio/libdeps/ESP-LCD/ESP32_IO_Expander/src/base/esp_io_expander.c \
+ C:/Users/Ifou/.platformio/packages/framework-arduinoespressif32@src-537c58760dafe7fcc8a1d9bbcf00b6f6/tools/sdk/esp32s3/include/esp_common/include/esp_bit_defs.h \
+ C:/Users/Ifou/.platformio/packages/framework-arduinoespressif32@src-537c58760dafe7fcc8a1d9bbcf00b6f6/tools/sdk/esp32s3/include/esp_common/include/esp_check.h \
+ C:/Users/Ifou/.platformio/packages/framework-arduinoespressif32@src-537c58760dafe7fcc8a1d9bbcf00b6f6/tools/sdk/esp32s3/include/esp_common/include/esp_err.h \
+ C:/Users/Ifou/.platformio/packages/framework-arduinoespressif32@src-537c58760dafe7fcc8a1d9bbcf00b6f6/tools/sdk/esp32s3/include/esp_common/include/esp_compiler.h \
+ C:/Users/Ifou/.platformio/packages/framework-arduinoespressif32@src-537c58760dafe7fcc8a1d9bbcf00b6f6/tools/sdk/esp32s3/include/log/include/esp_log.h \
+ C:/Users/Ifou/.platformio/packages/framework-arduinoespressif32@src-537c58760dafe7fcc8a1d9bbcf00b6f6/tools/sdk/esp32s3/include/esp_rom/include/esp_rom_sys.h \
+ C:/Users/Ifou/.platformio/packages/framework-arduinoespressif32@src-537c58760dafe7fcc8a1d9bbcf00b6f6/tools/sdk/esp32s3/include/soc/esp32s3/include/soc/reset_reasons.h \
+ C:/Users/Ifou/.platformio/packages/framework-arduinoespressif32@src-537c58760dafe7fcc8a1d9bbcf00b6f6/tools/sdk/esp32s3/include/esp_rom/include/esp32s3/rom/ets_sys.h \
+ C:/Users/Ifou/.platformio/packages/framework-arduinoespressif32@src-537c58760dafe7fcc8a1d9bbcf00b6f6/tools/sdk/esp32s3/include/soc/esp32s3/include/soc/soc.h \
+ C:/Users/Ifou/.platformio/packages/framework-arduinoespressif32@src-537c58760dafe7fcc8a1d9bbcf00b6f6/tools/sdk/esp32s3/include/esp_common/include/esp_assert.h \
+ C:/Users/Ifou/.platformio/packages/framework-arduinoespressif32@src-537c58760dafe7fcc8a1d9bbcf00b6f6/tools/sdk/esp32s3/include/log/include/esp_log_internal.h \
+ .pio/libdeps/ESP-LCD/ESP32_IO_Expander/src/base/esp_io_expander.h
---
+++ .pio/build/ESP-LCD/lib31a/ESP32_IO_Expander/private/CheckResult.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/ESP-LCD/lib31a/ESP32_IO_Expander/private/CheckResult.cpp.o: \
+ .pio/libdeps/ESP-LCD/ESP32_IO_Expander/src/private/CheckResult.cpp
